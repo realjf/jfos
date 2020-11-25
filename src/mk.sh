f99f93d3076573b87d383571851d9ab5be4d71bb
realjf <real_jf@hotmail.com>
2020-11-25 23:07:43 +0800

    update

--- src/mk.sh
+++ src/mk.sh
@@ -2,7 +2,7 @@
 as86 -o bs.o bs.s # bs.s file does not change
 bcc -c -ansi bc.c
 ld86 -d -o booter bs.o bc.o /usr/lib/bcc/libc.a
-dd if=booter of=/dev/fd0 bs=512 count=1 conv=notrunc
+dd if=booter of=booter.img bs=512 count=1 conv=notrunc
 
 # booting from floppy
-qemu-system-i386 -drive file=/dev/fd0,index=0,if=floppy -no-fd-bootchk
+qemu-system-i386 -fda ./booter.img -no-fd-bootchk
